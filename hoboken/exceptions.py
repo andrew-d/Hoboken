89bb79da7929e1a0ecefc143dae327772dbb66d9
Andrew Dunham <andrew_j_dunham@hotmail.com>
2012-05-28 02:03:38 +0200

    Added descriptions to exceptions

--- hoboken/exceptions.py
+++ hoboken/exceptions.py
@@ -1,8 +1,16 @@
 
 
 class HobokenException(Exception):
+    """Base class for all Hoboken exceptions."""
     pass
 
 
 class InvalidMatchTypeException(HobokenException):
+    """
+    Invalid matcher type given for a function.  Valid types are:
+      - A string, in either splat ("/blah/*/blah") or named-parameter
+        ("/blah/:param") format.
+      - A regex object (e.g. re.compile("/blah/(.+)"))
+      - A custom object that provides a match() callable.
+    """
     pass
