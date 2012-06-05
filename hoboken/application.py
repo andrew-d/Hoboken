6ddc8431efb37e190c41610e6eec4ca90ad6cf53
Andrew Dunham <andrew_j_dunham@hotmail.com>
2012-06-05 10:26:43 -0700

    Massive rewrite, added some tests

--- hoboken/application.py
+++ hoboken/application.py
@@ -1,37 +1,49 @@
-from functools import wraps
+# Future-proofing
+from __future__ import with_statement, absolute_import
 
+# Stdlib dependencies
+import re
+import urllib
+#from functools import wraps
+
+# External dependencies
 from webob import Request, Response
+from webob.exc import HTTPMethodNotAllowed, HTTPNotFound
 
+# In-package dependencies
 from .exceptions import *
 from .compat import *
 
 
-def _is_decorated(func):
+class WebRequest(Request):
     """
-    Check if the given function is decorated with an application decorator.
+    This class represents a request.  It is comprised of:
+     - The underlying WebOb Request object
+     - Parameters from the route matcher
+     - Instance variables, set by any before/after filters
     """
-    return func.func_dict.get("hoboken_wrapped", False)
+    def __init__(self, *args, **kwargs):
+        super(WebRequest, self).__init__(*args, **kwargs)
+        self.route_params = {}
 
-
-def _check_decorated(func):
-    """
-    Check if the given function is decorated with an application decorator,
-    and throw an error if not.
-    """
-
-    if not _is_decorated(func):
-        raise NotDecoratedException("Function %r not decorated!" % (func,))
+    def reinitialize(self):
+        # Reinitialize ourself.  For now, just clear route parameters.
+        self.route_params = {}
 
 
 class BasicMatcher(object):
     """
     Basic matcher - just checks if the path matches exactly.
     """
-    def __init__(self, path):
+    def __init__(self, path, case_sensitive=True):
         self.path = path
+        self.case_sensitive = case_sensitive
 
-    def match(self, request):
-        return self.path == request.path
+    def match(self, webr):
+        if self.case_sensitive:
+            return self.path == webr.path
+        else:
+            return self.path.lower() == webr.path.lower()
 
 
 class RegexMatcher(object):
@@ -39,76 +51,208 @@ class RegexMatcher(object):
     This class matches a URL using a provided regex.
     """
 
-    def __init__(self, re):
-        self.re = re
+    def __init__(self, regex, keys):
+        if isinstance(regex, RegexType):
+            self.re = regex
+        elif isinstance(regex, BaseStringType):
+            try:
+                self.re = re.compile(regex, flags)
+            except re.error:
+                raise TypeError("Parameter 'regex' is not a valid regex")
+        else:
+            raise TypeError("Parameter 'regex' is not a valid regex")
 
-    def match(self, request):
-        if self.re.match(request.path):
+        self.keys = keys
+
+    def match(self, webr):
+        match = self.re.match(webr.path)
+        if match:
+            matches = match.groups()
+
+            # Create lists for our captures / splat.
+            webr.route_params["captures"] = []
+            webr.route_params["splat"] = []
+
+            # Merge in the captures.
+            for k, v in zip(self.keys, matches):
+                if k in webr.route_params and isinstance(webr.route_params[k], list):
+                    webr.route_params[k].append(v)
+                else:
+                    webr.route_params[k] = v
+
             return True
         else:
             return False
 
-        # TODO: send matched groups to the function
-
 
 class HobokenApplication(object):
+    SUPPORTED_METHODS = ("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD")
+
     def __init__(self, name):
         self.name = name
-        self.routes = []        # Format: (method, matcher, function)
 
-        # TODO: Split routes into per-method routes, to speed up?
+        # Routes array. We split this by method, both for speed and simplicity.
+        # Format: List of tuples of the form (matcher, function)
+        self.routes = {}
 
-    def _decorate_function(self, func):
-        """
-        Decorate the given function to be a WSGI application, and add the
-        necessary logic for our application.
-        """
+        for m in self.SUPPORTED_METHODS:
+            self.routes[m] = []
 
-        # Mark the function as decorated.
-        func.func_dict['hoboken_wrapped'] = True
+    def add_route(self, method, match, func):
+        # Methods are uppercase.
+        method = method.upper()
 
-        # This decorator makes the function a WSGI application.
-        @wraps(func)
-        def wrapper(environ, start_response, *args, **kwargs):
-            # Create our WebOb request.
-            req = Request(environ)
+        # Check for valid method.
+        if not method in self.SUPPORTED_METHODS:
+            raise HobokenException("Invalid method type given: %s" % (method,))
 
-            # Call wrapped function.
-            retval = func(req, *args, **kwargs)
-
-            # Make a WSGI response.
-            resp = Response(body=retval)
-
-            # Return our actual response.
-            return resp(environ, start_response)
-
-        return wrapper
-
-    def _make_decorator(self, method, match):
-        # TODO: decorate me!
-        func = method
-
+        # Determine match type.
         if isinstance(match, BaseStringType):
-            # Parse "match" to determine if splat-style, param-style, or bare.
-            if match.find(":") != -1 or match.find("*") != -1:
-                # Param/splat style
-                pass
-            else:
-                # Bare.  Create matcher, return it.
-                self.routes.append((method, BasicMatcher(match), func))
+            # Param/splat style.  We need to extract the splats, the named
+            # parameters, and then create a regex to match it.
+            #
+            # The general rules are as follows:
+            #  - Block parameters like :block match one path segment -
+            #    i.e. until the next "/", special character, or end-of-
+            #    string.  Note that blocks must match at least one
+            #    character.
+            #  - Splats match anything, but always match non-greedily.
+            #    Splats can also match the empty string (i.e. nothing).
+            #
+            # So, we convert to a regex in the following way:
+            #  - Blocks are converted like this:
+            #      blah:block --> r"blah([^/?#]+)"
+            #  - Splats are converted like this:
+            #      blah*blah  --> r"blah(.*?)blah"
+
+            keys = []
+
+            def convert_match(match):
+                if match.group(0) == '*':
+                    keys.append("splat")
+                    return r"(.*?)"
+                else:
+                    keys.append(match.group(0)[1:])
+                    return r"([^/?#]+)"
+
+            # First we encode special characters.  The helper function will
+            # return a regex that matches that character and any modifications.
+            def encode_character(char):
+                if char == ' ':
+                    return "(?: |" + encode_character("+") + ")"
+                else:
+                    encoded = urllib.quote(char)
+
+                    # If the encoded version is unchanged, then we match both
+                    # the bare version, along with the encoded version.
+                    if encoded == char:
+                        encoded = "(?:" + re.escape(char) + "|" + re.escape("%" + hex(ord(char))[2:]) + ")"
+
+                    return encoded
+
+            # Encode everything that's not in the set:
+            #   [?%\/:*] + all alphanumeric characters + underscore.
+            encoded_match = re.sub(r"[^?%\\/:*\w]", encode_character, match)
+
+            # Now, replace parameters or splats with their matching regex.
+            match_regex = re.sub(r"((:\w+)|\*)", convert_match, encoded_match)
+
+            # Done - add the route.
+            self.routes[method].append((RegexMatcher(match_regex, keys), func))
+
         elif isinstance(match, RegexType):
             # match is a regex, so we just add it as-is.
-            self.routes.append((method, RegexMatcher(match), func))
+            self.routes[method].append((RegexMatcher(match), func))
+
         elif hasattr(match, "match") and iscallable(getattr(match, "match")):
             # Don't know what type it is, but it has a callable "match"
             # attribute, so we use that.
-            self.routes.append((method, match, func))
+            self.routes[method].append((match, func))
+
         else:
             # Unknown type!
             raise InvalidMatchTypeException("Unknown type: %r" % (match,))
 
+    def _decorate_and_route(self, method, match):
+        def internal_decorator(func):
+            self.add_route(method, match, func)
+            return func
+        return internal_decorator
+
     def get(self, match):
-        return _make_decorator("GET", match)
+        return self._decorate_and_route("GET", match)
 
     def post(self, match):
-        return _make_decorator("POST", match)
+        return self._decorate_and_route("POST", match)
+
+    def put(self, match):
+        return self._decorate_and_route("PUT", match)
+
+    def delete(self, match):
+        return self._decorate_and_route("DELETE", match)
+
+    def head(self, match):
+        return self._decorate_and_route("HEAD", match)
+
+    def options(self, match):
+        return self._decorate_and_route("OPTIONS", match)
+
+    def patch(self, match):
+        return self._decorate_and_route("PATCH", match)
+
+    def wsgi_entrypoint(self, environ, start_response):
+        # Create our request object.
+        req = WebRequest(environ)
+
+        # Check for valid method.
+        if req.method not in self.SUPPORTED_METHODS:
+            # Send "invalid method" exception.
+            exc = HTTPMethodNotAllowed(location=environ.path)
+            resp = req.get_response(exc)
+            return resp(environ, start_response)
+
+        # Create response.
+        resp = Response()
+
+        # For each route of the specified type, try to match it.
+        matched = False
+        for matcher, func in self.routes[req.method]:
+            if matcher.match(req):
+                matched = True
+
+                try:
+                    ret = func(req, resp)
+
+                    # If we have a non-None return value, we set the body of
+                    # the response to this value.
+                    if ret is not None:
+                        resp.body = ret
+                except ContinueRoutingException:
+                    # Reinitialize the request, which clears our route params.
+                    matched = False
+                    req.reinitialize()
+
+                except HaltRoutingException:
+                    # TODO: Halt routing.
+                    pass
+
+                else:
+                    # The 'else' clause is executed when NO exception has been
+                    # raised.  This signifies that all has gone right, and thus
+                    # we should continue to process "after()" calls, and so on.
+
+                    # TODO: Fill me in.
+                    pass
+
+                # Stop processing this request - we're done.
+                break
+
+        if not matched:
+            # Return a 404 request.
+            exc = HTTPNotFound(location=req.path)
+            resp = req.get_response(exc)
+
+        return resp(environ, start_response)
+
+    def __call__(self, environ, start_response):
+        return self.wsgi_entrypoint(environ, start_response)
--- hoboken/exceptions.py
+++ hoboken/exceptions.py
@@ -22,3 +22,36 @@ class NotDecoratedException(HobokenException):
     function that hasn't been decorated with a route.
     """
     pass
+
+
+class HobokenUserException(HobokenException):
+    """Base class for all user-raise-able exceptions."""
+    pass
+
+
+class ContinueRoutingException(HobokenUserException):
+    """
+    This exception signals that Hoboken should continue routing the current
+    request.  The current route is ignored, and the next one is tried.
+    """
+    pass
+
+
+class HaltRoutingException(HobokenUserException):
+    """
+    This exception signals that Hoboken should stop routing the current
+    request.  after() calls are NOT called, and the request is finished
+    immediately.
+    """
+    # TODO: Should after() calls be called?
+    pass
+
+
+class RedirectException(HobokenUserException):
+    """
+    This exception signals a redirect to another location.  Unless the
+    'redirect_type' argument is given, defaults to a 302 redirect.
+    """
+    def __init__(self, *args, **kwargs):
+        self.redirect_type = kwargs.get("redirect_type", 302)
+        super(RedirectException, self).__init__(*args, **kwargs)
