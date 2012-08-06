aba1584aa5dd11e9c30e0cff2755b2e082d05db7
Andrew Dunham <andrew_j_dunham@hotmail.com>
2012-08-06 11:21:00 -0700

    Split request/response objects out to their own file

--- hoboken/application.py
+++ hoboken/application.py
@@ -14,12 +14,12 @@ except:                     # pragma: no cover
 #from functools import wraps
 
 # External dependencies
-import webob
 from webob.exc import HTTPMethodNotAllowed, HTTPNotFound, HTTPInternalServerError
 
 # In-package dependencies
 from .exceptions import *
 from .matchers import *
+from .objects import *
 from .compat import *
 
 
@@ -41,55 +41,6 @@ def set_func_attr(func, attr, value):                           # pragma: no cov
         func_dict = func.func_dict
 
     func_dict[attr] = value
-
-
-class Request(webob.BaseRequest):
-    """
-    Hoboken's request objects.  Uses WebOb's request object
-    """
-    @property
-    def is_safe(self):
-        """
-        Returns True if the request is considered "safe" - i.e. if the request
-        should be treated as not modifying any state.
-        """
-        return self.method in ['GET', 'HEAD', 'OPTIONS', 'TRACE']
-
-    @property
-    def is_idempotent(self):
-        """
-        Returns True if the request is considered idempotent - i.e. if two
-        successive identical requests should result in the same result.
-        """
-        return self.is_safe or self.method in ['PUT', 'DELETE']
-
-class Response(webob.Response):
-    """
-    Hoboken's request objects.  Uses WebOb's request object.
-    """
-    @property
-    def is_informational(self):
-        return 100 <= self.status_int <= 199
-
-    @property
-    def is_success(self):
-        return 200 <= self.status_int <= 299
-
-    @property
-    def is_redirect(self):
-        return 300 <= self.status_int <= 399
-
-    @property
-    def is_client_error(self):
-        return 400 <= self.status_int <= 499
-
-    @property
-    def is_server_error(self):
-        return 500 <= self.status_int <= 599
-
-    @property
-    def is_not_found(self):
-        return self.status_int == 404
 
 
 def condition(condition_func):
@@ -125,7 +76,6 @@ def pass_route():
     will be returned.
     """
     raise ContinueRoutingException()
-
 
 
 class Route(object):
---
+++ hoboken/objects.py
@@ -0,0 +1,54 @@
+# Future-proofing
+from __future__ import with_statement, absolute_import, print_function
+
+import webob
+
+class Request(webob.BaseRequest):
+    """
+    Hoboken's request objects.  Uses WebOb's request object
+    """
+    @property
+    def is_safe(self):
+        """
+        Returns True if the request is considered "safe" - i.e. if the request
+        should be treated as not modifying any state.
+        """
+        return self.method in ['GET', 'HEAD', 'OPTIONS', 'TRACE']
+
+    @property
+    def is_idempotent(self):
+        """
+        Returns True if the request is considered idempotent - i.e. if two
+        successive identical requests should result in the same result.
+        """
+        return self.is_safe or self.method in ['PUT', 'DELETE']
+
+class Response(webob.Response):
+    """
+    Hoboken's request objects.  Uses WebOb's request object.
+    """
+    @property
+    def is_informational(self):
+        return 100 <= self.status_int <= 199
+
+    @property
+    def is_success(self):
+        return 200 <= self.status_int <= 299
+
+    @property
+    def is_redirect(self):
+        return 300 <= self.status_int <= 399
+
+    @property
+    def is_client_error(self):
+        return 400 <= self.status_int <= 499
+
+    @property
+    def is_server_error(self):
+        return 500 <= self.status_int <= 599
+
+    @property
+    def is_not_found(self):
+        return self.status_int == 404
+
+
