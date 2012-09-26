03821ff780bc19dbdee98f76d9e80bd5b262d649
Andrew Dunham <andrew_j_dunham@hotmail.com>
2012-09-26 11:12:55 -0400

    Add TODO list for cache mixin

--- hoboken/objects/mixins/cache.py
+++ hoboken/objects/mixins/cache.py
@@ -6,3 +6,12 @@ class WSGICacheMixin(object):
     def __init__(self, *args, **kwargs):
         super(WSGIAcceptMixin, self).__init__(*args, **kwargs)
 
+    # TODO:
+    #   - Need to handle request vs. response directives
+    #   - Accessors like, e.g.: response.max_age = 1234, or request.no_cache = True
+    #   - The 'Age' HTTP header
+    #   - The 'Expires' HTTP header
+    #   - 'Pragma: no-cache' --> 'Cache-Control: no-cache'
+    #   - The 'Vary' HTTP header
+    #   - 
+
