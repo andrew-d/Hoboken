ea7ae85f77bf65ba8759d162efd213447bbbd22d
Andrew Dunham <andrew@du.nham.ca>
2012-10-09 09:02:05 -0400

    Add more tests for the Accept mixin

--- hoboken/tests/objects/mixins_accept.py
+++ hoboken/tests/objects/mixins_accept.py
@@ -93,9 +93,72 @@ class TestMIMEAccept(BaseTestCase):
         self.assert_false(self.m._match(b"foo/bar", b"bar/baz"))
 
 
+class TestLanguageAccept(BaseTestCase):
+    def setup(self):
+        self.l = LanguageAccept()
+
+    def test_simple_match(self):
+        self.assert_true(self.l._match("en-US", "en-US"))
+
+    def test_normalized_match(self):
+        self.assert_true(self.l._match("en_US", "EN-us"))
+
+    def test_wildcard_match(self):
+        self.assert_true(self.l._match("en-US", "*"))
+
+
+class TestCharsetAccept(BaseTestCase):
+    def setup(self):
+        self.c = CharsetAccept()
+
+    def test_simple_match(self):
+        self.assert_true(self.c._match("utf-8", "utf-8"))
+
+    def test_normalize_match(self):
+        self.assert_true(self.c._match("utf-8", "UTF8"))
+
+    def test_wildcard_match(self):
+        self.assert_true(self.c._match("utf-8", "*"))
+
+
+class TestWSGIAcceptMixin(BaseTestCase):
+    def make_obj(self, types):
+        class TestObject(object):
+            accept_mimetypes = []
+
+        self.o = TestObject()
+        self.o.accept_mimetypes = types
+
+    def test_accepts_json(self):
+        self.make_obj([b'application/json'])
+        self.assert_true(WSGIAcceptMixin.accepts_json.__get__(self.o))
+
+        self.make_obj([b'application/other'])
+        self.assert_false(WSGIAcceptMixin.accepts_json.__get__(self.o))
+
+    def test_accepts_xhtml(self):
+        self.make_obj([b'application/xml'])
+        self.assert_true(WSGIAcceptMixin.accepts_xhtml.__get__(self.o))
+
+        self.make_obj([b'application/other'])
+        self.assert_false(WSGIAcceptMixin.accepts_xhtml.__get__(self.o))
+
+    def test_accepts_xhtml(self):
+        self.make_obj([b'text/html'])
+        self.o.accepts_xhtml = False
+        self.assert_true(WSGIAcceptMixin.accepts_html.__get__(self.o))
+
+        self.make_obj([b'text/plain'])
+        self.o.accepts_xhtml = False
+        self.assert_false(WSGIAcceptMixin.accepts_html.__get__(self.o))
+
+
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(TestAcceptList))
     suite.addTest(unittest.makeSuite(TestMIMEAccept))
+    suite.addTest(unittest.makeSuite(TestLanguageAccept))
+    suite.addTest(unittest.makeSuite(TestCharsetAccept))
+    suite.addTest(unittest.makeSuite(TestWSGIAcceptMixin))
 
     return suite
